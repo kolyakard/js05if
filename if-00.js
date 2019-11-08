bf229416bd6ffc8ee8e2034bbe53bf5bc30f3c41
kolyakard <54824922+kolyakard@users.noreply.github.com>
2019-11-08 14:09:40 +0900

    Update if-00.js

--- if-00.js
+++ if-00.js
@@ -4,9 +4,9 @@
 function f(x, y, z){
     if (x+y+z < x*y*z && x+y+z < x*y+z)
         return x+y+z;
-    if (x*y*z < x+y+z && x*y*z < x*y+z)
+    else if (x*y*z < x+y+z && x*y*z < x*y+z)
         return x*y*z; 
-    return x*y + z;
+    else return x*y + z;
 }
 
 module.exports = f;
